ce5878982595a00ab5f9889051d165340039236a
juljaVysotska <julja.vysotska@gmail.com>
2019-10-21 16:00:01 +0300

    lesson 10
    task 2

--- lesson-10/2.js
+++ lesson-10/2.js
@@ -20,11 +20,19 @@
 // Решение
 
 
-function calculate(...arg) {
+function calculate(first, ...rest) {
 
-	if(typeof arguments !== 'function'){
-		throw new TypeError('Some of your argument is not a function');
-	}
+    if (typeof first !== 'function') {
+        throw new TypeError('Some of your argument is not a function');
+    }
+
+    let calcVal = first();
+
+    for (let key in rest) {
+        const func = rest[key];
+        calcVal = func(calcVal);
+    }
+    return calcVal;
 }
 
 const result = calculate(
@@ -37,8 +45,11 @@ const result = calculate(
     prevResult => {
         return prevResult * 5;
     },
+    prevResult => {
+        return prevResult + 10;
+    },
 );
 
-console.log(result); // 55
+console.log(result); // 65
 
 exports.calculate = calculate;
